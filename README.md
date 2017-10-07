5868a6cedfb8551a8761a3376144716426910d1f
vwillcox <talking@talktech.info>
2017-10-07 17:26:18 +0100

    Headless mode - run without a screen, keyboard or mouse. Reboot and power off options added

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # papirus-netapp
 A Network appliance for the PaPiRus from PiSupply
 
-I have now cleaned up some of the code.
+I have now cleaned up some of the code and added a new menu to run the Appliance headless.
 
 Recent Changes (7th October 2017)
 
 1) Added code to fetch the runtime path, now you can run this from a directory of your choosing.
 2) Fixed some of the display code so it looks a little better when running
 3) Added install.sh - So you can now clone the repo and run sudo ./install.sh to get all the python libraries installed 
-
+4) Headless mode added - See Menu Structure below
 
 ------
 # Using this App
@@ -17,7 +17,34 @@ Recent Changes (7th October 2017)
 2.1) You can now sudo ./install.sh to install everything you need on the Raspberry Pi.
 3) Run python main.py
 
+# Running the Applicance code on Raspberry Pi Reboot
+
+```shell
+sudo crontab -e
+```
+
+add a line like
+
+```
+@reboot python /home/pi/papirus-netapp/main.py
+```
+
+change the path to match where you cloned the repo too.
+
+
+-----
+
 This assumes that you have already installed all the PaPiRus bits and have the screen working on your Raspberry Pi.
+
+# Menu structure
+* 1) Go to Advanced Menu
+* 2) Scan for Raspberry Pi's
+* 3) Show Speed Graph
+* 4) Run a speed test
+# Advanced Menu
+* 1) Back to Main Menu
+* 2) Power off the Appliance
+* 3) Reboot the Appliance
 
 
 -----
--- main.py
+++ main.py
@@ -10,15 +10,26 @@ import ImageFont
 from gpiozero import Button
 import ImageDemo
 import scan4pi
+from time import sleep
+
 
 def show_menu():
 	screen.Clear()
 	screen.AddText("Main Menu", 10, 10)
-	screen.AddText("1) Main Menu", 10, 40)
+	screen.AddText("1) Advanced Menu", 10, 40)
 	screen.AddText("2) Scan 4 Pi's", 10, 70)
 	screen.AddText("3) Speed Graph", 10, 100)
 	screen.AddText("4) Speed Test", 10, 130)
 	screen.WriteAll()
+
+def show_menu_2():
+        screen.Clear()
+        screen.AddText("Advanced Menu", 10, 10)
+        screen.AddText("1) Main Menu", 10, 40)
+        screen.AddText("2) Power Off Pi", 10, 70)
+        screen.AddText("3) Reboot Pi", 10, 100)
+        screen.WriteAll()
+
 
 def speed_test():
 	screen.Clear()
@@ -47,6 +58,23 @@ def graph_it():
 def scan4it():
 	scan4pi.main()
 
+def reboot_it():
+	screen.Clear()
+        screen.AddText("Rebooting", 20, 70, size2)
+        screen.WriteAll()
+	sleep(2)
+	screen.Clear()
+	os.system('sudo shutdown -r now')
+
+def power_off():
+        screen.Clear()
+        screen.AddText("Shutting Down", 20, 70, size2)
+        screen.WriteAll()
+        sleep(2)
+        screen.Clear()
+	os.system('sudo shutdown now')
+
+
 if "__main__" == __name__:
 	possible_fonts = [
         '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf',            # R.Pi
@@ -62,7 +90,7 @@ if "__main__" == __name__:
 
         if '' == FONT_FILE:
                 raise 'no font file found'
-
+	menu = 0
         size1 = 11
         size2 = 17
 	dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -84,14 +112,28 @@ if "__main__" == __name__:
 	show_menu()
 	try:
 		while True:
-			if button1.wait_for_release(1):
+			if (menu == 0 and button1.wait_for_release(1)):
 				speed_test()
-			if button2.wait_for_release(1):
+				menu = 1
+			if (menu == 1 and button1.wait_for_release(1)):
+				power_off()
+				menu = 0
+			if (menu == 0 and button2.wait_for_release(1)):
 				graph_it()
-			if button3.wait_for_release(1):
+				menu = 1
+			if (menu == 1 and button2.wait_for_release(1)):
+				reboot_it()
+				menu = 0
+			if (menu == 0 and button3.wait_for_release(1)):
 				scan4it()
-			if button4.wait_for_release(1):
+				menu = 1
+			if (menu == 0 and button4.wait_for_release(1)):
+				show_menu_2()
+				menu = 1
+			if (menu == 1 and button4.wait_for_release(1)):
 				show_menu()
+				menu = 0
+
 	except KeyboardInterrupt:
 		print ("Exiting Appliance Code")
 		screen.Clear()
